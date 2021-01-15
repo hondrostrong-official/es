e260609289fce87915b7bb87aca29b0cd902f2f0
hondrostrong-official <76270734+hondrostrong-official@users.noreply.github.com>
2021-01-15 11:02:38 +0200

    1

---
+++ js/translater.js
@@ -0,0 +1,193 @@
+var new_lang = {
+
+"lt0": "¡DESARROLLO INNOVADOR!",
+
+"lt1": "HONDRO<span>STRONG</span>",
+
+"lt2": "¡En un 99% de casos <span>recupera los tejidos </span>de articulaciones damnificadas!",
+
+"lt3": "<span>Para el dolor </span>en 10 minutos.",
+
+"lt4": "¡Recupera la movilidad de articulaciones <span>en 14 días!</span>",
+
+"lt5": "<span>Saneamiento y mantenimiento de articulaciones </span>a micro nivel.",
+
+"lt6": "¡Un formula patentada,<span>no tiene análogos!</span>",
+
+"lt7": "¡OFERTA ESPECIAL!",
+
+"lt8": "hasta el final de la oferta quedan:",
+
+"lt9": "horas",
+
+"lt10": "minutos",
+
+"lt11": "segundos",
+
+"lt12": "Elija el país",
+
+"lt13": "Introduzca el nombre:",
+
+"lt14": "Introduzca el teléfono:",
+
+"lt15": "Se quedaron mercancías en oferta:<span class=\"pack_count\">7</span>",
+
+"lt16": "¡DESTRUCCIÓN DE ARTICULACIONES:",
+
+"lt17": "CADA UNO DE NOSOTROS ESTÁ EN LA ZONA DE RIESGO!",
+
+"lt18": "EDAD<span>MAYOR DE 35 AÑOS</span>",
+
+"lt19": "CLÍMAX<span>EN MUJERES</span>",
+
+"lt20": "SOBREPESO Y<span>POCA MOVILIDAD </span>",
+
+"lt21": "<span>EJERCICIO FÍSICO</span>ELEVADO",
+
+"lt22": "INFECCIONES",
+
+"lt23": "TRAUMAS",
+
+"lt24": "¡CADA UNO DE ESTOS FACTORES MATA ARTICULACIONES!",
+
+"lt25": "HONDRO<span>STRONG</span>",
+
+"lt26": "El único fármaco nanotecnológico que recupera articulaciones de modo natural.",
+
+"lt27": "Hondrostrong es una bio-crema nativa, un complejo natural fortalecido con microesferas de components bio-activos.",
+
+"lt28": "Tiene un efecto individual dirigido al tejido cartílaginoso y conjuntivo de articulaciones, músculos contiguous y vasos sanguíneos.",
+
+"lt29": "EL DESARROLLO MÉDICO NACIONAL ÚNICO DESCLASIFICADO ",
+
+"lt30": "CERTIFICADO DE ARCHIVO",
+
+"lt31": "Desde los años 1960 del siglo XX se aplicaba por cosmonáutas cuyos articulaciones estaban expuestas a los cambios degenerativos en las condiciones de ingravidez.",
+
+"lt32": "<span>El fármaco prevenía todos los procesos patológicos en la cápsula articular </span>en caso de trastornos de metabolismo, ejercicio físico elevado y factor profesional del cambio patologico de articulaciones.",
+
+"lt33": "CUANDO SE PRESCRIBE ",
+
+"lt34": "HONDRO<span>STRONG</span>",
+
+"lt35": "En caso de cualesquiera enfermedades del tejido articular, cartílaginoso o óseo<span>(enfermedades inflamatorias e infeciosas, lesines degenerativas)</span>",
+
+"lt36": "Para la prevención del desarrollo de enfermedades articulares <span>(está demostrado: con la edad la salud y la movilidad de articulaciones se reduce drásticamente)</span>",
+
+"lt37": "<span>Para los deportistas,</span>personas que se encuentran bajo altas tensiones físicas",
+
+"lt38": "¡En caso de la vida sedentaria y presencia del sobrepeso<span>(que crea un impacto elevado en articulaciones!)</span>",
+
+"lt39": "Después de traumas y operaciones en articulaciones",
+
+"lt40": "PEDIR EN OFERTA",
+
+"lt41": "como funciona",
+
+"lt42": "HONDRO<span>STRONG</span>",
+
+"lt43": "<span>¡Elimina el dolor</span>en un instante!",
+
+"lt44": "Mejora la elasticidad de los vasos sanguíneos <span>y garantiza el suministro de nutrientes hacia articulaciones</span>",
+
+"lt45": "Previene el síndrome doloroso en la zona de articulaciones, espalda,<span>provocado por factores neuróticos, inflamadores, estresantes </span>",
+
+"lt46": "Aumenta poblaciones de células jóvenes sanas <span>de condrocitas del tejido cartílaginoso en articulaciones</span>",
+
+"lt47": "Protege el cartílago <span>contra el afinamiento</span>",
+
+"lt48": "Elimina la infección",
+
+"lt49": "Elimina calambres de músculos gastrocnemios ",
+
+"lt50": "Previene procesos congestivos <span>y edemas de articulaciones</span>",
+
+"lt51": "TECNOLOGÍA <br><span>ARTROrevival</span>",
+
+"lt52": "¡Gracias a desarrollos innovadores en la esfera de bionanotecnologías,<span>los componentes de HONDROSTRONG alcanzan los tejidos de articulaciones afectadas de una forma increiblemente rápida y tienen un efecto recuperador </span>10 veces más potente que todos los fármacos existentes actualmente!",
+
+"lt53": "Los componentes de la bio-crema se diluyen totalmente en líquidos celulares que les ayuda a alcanzar zonas afectadas más rápidamente y integrarse en la bio-matriz de la articulación regenerándola.",
+
+"lt54": "x10",
+
+"lt55": "¡les ha ayudado ya",
+
+"lt56": "HONDRO<span>STRONG!</span>",
+
+"lt57": "artritis",
+
+"lt58": "Hace dos años me hicieron un diagnóstico desconsolador – la artritis reumatoidea. No hay palabras para describir como me asusté. Es que siempre sabía que un diagnóstico así es un billete solo de ida, ya que la enfermedad solamente se estará acentuando con el tiempo. Pero mi médico me calmó en seguida al decir que en el siglo 21 la artritis se curaba con éxito por medio de HONDROSTRONG. Y de verdad, durante un mes del uso sentí un mejoramiento considerable: desaparecieron dolores, se mejoró la movilidad de articulaciones, los médicos registran una dinámica positiva. ¡Ahora siempre tengo HONDROSTRONG al alcance de la mano!",
+
+"lt59": "Olaya, 51 año",
+
+"lt60": "DOLOR EN LA CINTURA",
+
+"lt61": "Soy powerlifter profesional, una barra de discos de 180 kilos para mí es un peso de trabajo. Toda la vida yo estaba persiguiendo el progreso, no pensaba en mi salud. La sitación ha empeorado tanto que justo antes de competiciones importantes mi columna vertebral se ha destrozado. Un dolor agudo en la cintura, un edema, sin hablar de hacer sentadiallas, NI DESDOBLARME PODÍA, me estaba salvando con analgésicos no más. Mi entrenador me ha dado una buena paliza, pero he tenido suerte. Por su consejo sin demora he comenzado a usar HONDROSTRONG. Si no hubiera sido por este remedio, lo mas probable, habría terminado mi carrera deportiva. En 2 semanas he logrado normalizar mi espalda e intervenir con éxito. Ahora me estoy rehabilitando activamente.¡HONDROSTRONG ayuda perfectamente!",
+
+"lt62": "Ingvar, 28 años",
+
+"lt63": "machucón de la rodilla",
+
+"lt64": "Me he caido mal, y el golpe ha sido justo en la rodilla. La radiografía ha mostrado una grieta en la articulación. ¡No estaba preparada para eso, es que una trauma así es el régimen de cama durante unas tres semanas como mínimo! Y mis vacaciones están delante de las narices. He tenido que tomar medidas urgentes. Por consejo de mis próximos he pedido HONDROSTRONG. Gracias a él la grieta se ha curado aceleradamente en dos semanas, y he podido ir al mar tranquilamente. Ahora recomiendo a todos este remedio maravilloso.",
+
+"lt65": "Ximena, 24 años",
+
+"lt66": "¡CUIDADO CON FALSIFICACIONES!",
+
+"lt67": "Para evitar falsificaciones y análogos de baja calidad compre el producto original solamente en este sitio web.",
+
+"lt68": "¡100% DE CALIDAD ESTÁ GARANTIZADO POR EL PRODUCTOR!",
+
+"lt69": "cómo trabajamos:",
+
+"lt70": "Formalice un pedido instantáneo y pida un descuento ",
+
+"lt71": "Espere la llamada del cosultor para acordar los detalles",
+
+"lt72": "El pago es solamente después de la recepción de la mercancía",
+
+"lt73": "¡DESARROLLO INNOVADOR!",
+
+"lt74": "HONDRO<span>STRONG</span>",
+
+"lt75": "¡En un 99% de casos <span>recupera los tejidos </span>de articulaciones afectadas!",
+
+"lt76": "<span>Parar el dolor</span>en 10 días.",
+
+"lt77": "¡Recupera la movilidad de articulaciones <span>en 14 días!</span>",
+
+"lt78": "<span>El saneamiento y mantenimiento de articulaciones </span>a micro nivel.",
+
+"lt79": "¡Una fórmula patentada<span>que no tiene análogos!</span>",
+
+"lt80": "¡OFERTA ESPECIAL!",
+
+"lt81": "hasta el final de la oferta quedan:",
+
+"lt82": "horas",
+
+"lt83": "minutos",
+
+"lt84": "segundos",
+
+"lt85": "Elija el país",
+
+"lt86": "Introduzca el nombre:",
+
+"lt87": "Introduzca el teléfono:",
+
+"lt88": "Se qedaron mercancías en oferta:<span class=\"pack_count\">7</span>",
+
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
